6eed7cb2dcba1c9276427d7457802e5789c3b28a
Lilium <19245343+EndlesslyFlowering@users.noreply.github.com>
2023-05-21 22:04:39 +0200

    Merge pull request #1 from WallSoGB/upgrade-toggle: [d3d9] Add functions to control RT upgrades from outside and change variable names to my liking

--- src/d3d9/d3d9_common_texture.h
+++ src/d3d9/d3d9_common_texture.h
@@ -490,7 +490,7 @@ namespace dxvk {
 
     ID3D9VkInteropTexture* GetVkInterop() { return &m_d3d9Interop; }
 
-    static bool forceDisableUpgrade;
+    static bool forceDisableRenderTargetUpgrade;
 
   private:
 
