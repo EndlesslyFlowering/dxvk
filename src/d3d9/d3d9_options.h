2ddf9553a2dc0797b7a3ec55646f9b102c9abefa
Lilium <EndlesslyFlowering@protonmail.com>
2023-06-22 01:05:23 +0200

    [d3d9] final? fix up for format upgrades

--- src/d3d9/d3d9_options.h
+++ src/d3d9/d3d9_options.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include <d3d9.h>
 
 #include "../util/config/config.h"
@@ -12,6 +14,10 @@ namespace dxvk {
     Enabled  = 1,
     Strict   = 2
   };
+
+  bool IsSensibleFormatUpgrade(
+    const D3DFORMAT OriginalFormat,
+    const D3DFORMAT UpgradedFormat);
 
   struct D3D9Options {
 
@@ -148,42 +154,44 @@ namespace dxvk {
     bool clampNegativeLodBias;
 
     /// upgrades render targets
-    bool enableRenderTargetUpgrade;
+    bool enableRenderTargetUpgrades;
+
+    /// array that describes which format will be ugprade to which
+    std::array<D3DFORMAT, 200> formatUpgradeArray;
 
     /// upgrade specific render target formats
-    D3DFORMAT upgrade_RGBA8_To;
-    D3DFORMAT upgrade_RGBX8_To;
-    D3DFORMAT upgrade_BGRA8_To;
-    D3DFORMAT upgrade_BGRX8_To;
-    D3DFORMAT upgrade_RGB10A2_To;
-    D3DFORMAT upgrade_BGR10A2_To;
-    D3DFORMAT upgrade_RGBA16_To;
-    D3DFORMAT upgrade_RGBA16F_To;
+    //D3DFORMAT upgrade_RGBA8_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_RGBX8_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_BGRA8_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_BGRX8_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_RGB10A2_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_BGR10A2_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_RGBA16_UNORM_renderTargetTo;
+    //D3DFORMAT upgrade_RGBA16_FLOAT_renderTargetTo;
 
-    /// enable upgrade swapchain
-    bool enableSwapchainUpgrade;
+    /// enable back buffer format upgrade
+    bool enableBackBufferFormatUpgrade;
+
+    /// upgrade back buffer format to
+    D3DFORMAT upgradeBackBufferFormatTo;
+
+    /// enable upgrade swap chain
+    bool enableSwapChainUpgrade;
 
     /// which output format to upgrade to
-    VkFormat upgradeSwapchainFormatTo;
+    VkFormat upgradeSwapChainFormatTo;
 
     /// output color space to upgrade to
-    VkColorSpaceKHR upgradeSwapchainColorSpaceTo;
+    VkColorSpaceKHR upgradeSwapChainColorSpaceTo;
 
-    /// Upgrade output format in the virtual D3D9 swapchain
-    /// may or may not cause issues. Basically cosmetics ;)
-    bool enableSwapchainFormatUpgradeInternal;
+    /// disables 
+    //bool disableSrgbSamplingAndWriting;
 
-    /// which internal output format upgrade to
-    D3DFORMAT upgradeSwapchainFormatInternalTo;
-
-    /// log formats used
+    /// log render target formats used
     bool logRenderTargetFormatsUsed;
 
-    /// log formats used
-    bool logFormatsUsed;
-
     /// enfore fullscreen exclusive or windowed mode
-    /// for the internal swapchain
+    /// for the internal swap chain
     bool enforceWindowModeInternally;
 #if defined(_MSC_VER)
     BOOL
--- src/d3d9/d3d9_swapchain.cpp
+++ src/d3d9/d3d9_swapchain.cpp
@@ -1151,40 +1151,103 @@ namespace dxvk {
     return maxFrameLatency;
   }
 
+  void D3D9SwapChainEx::SwapChainUpgradeLogger(
+    const VkFormat        OriginalFormat,
+    const VkFormat        UpgradedFormat,
+    const VkColorSpaceKHR UpgradedColorSpace) {
+
+    Logger::info(str::format("DXVK (D3D9): swap chain upgrade:\n",
+                             "                 from: ", OriginalFormat, " + ", VK_COLOR_SPACE_SRGB_NONLINEAR_KHR, "\n",
+                             "                 to:   ", UpgradedFormat, " + ", UpgradedColorSpace));
+    return;
+  }
 
   VkSurfaceFormatKHR D3D9SwapChainEx::GetSurfaceFormat() {
     D3D9Format format = EnumerateFormat(m_presentParams.BackBufferFormat);
 
-    VkFormat        ugFmtTo = m_parent->GetOptions()->upgradeSwapchainFormatTo;
-    VkColorSpaceKHR ugCspTo = m_parent->GetOptions()->upgradeSwapchainColorSpaceTo;
+    VkFormat        upgradeFormatTo     = m_parent->GetOptions()->upgradeSwapChainFormatTo;
+    VkColorSpaceKHR upgradeColorSpaceTo = m_parent->GetOptions()->upgradeSwapChainColorSpaceTo;
+
+    if (upgradeColorSpaceTo == VK_COLOR_SPACE_MAX_ENUM_KHR) {
+      upgradeColorSpaceTo = m_colorspace;
+    }
+
+#define SWAP_CHAIN_UPGRADE_THROW_ERROR(OriginalFormat)                                                             \
+          throw DxvkError(str::format("DXVK (D3D9): No suitable swap chain upgrade combination found!\n",          \
+                                          "             planned upgrade:\n",                                       \
+                                          "                 from: ", OriginalFormat,  " + ", m_colorspace, "\n",   \
+                                          "                 to:   ", upgradeFormatTo, " + ", upgradeColorSpaceTo))
+
 
     switch (format) {
       default:
         Logger::warn(str::format("D3D9SwapChainEx: Unexpected format: ", format));
         [[fallthrough]];
 
-      case D3D9Format::A8R8G8B8:
-      case D3D9Format::X8R8G8B8: {
-        if (m_parent->GetOptions()->enableSwapchainUpgrade
-         && (ugFmtTo == VK_FORMAT_R16G16B16A16_SFLOAT
-          || ugFmtTo == VK_FORMAT_R16G16B16A16_UNORM
-          || ugFmtTo == VK_FORMAT_A2B10G10R10_UNORM_PACK32
-          || ugFmtTo == VK_FORMAT_A2R10G10B10_UNORM_PACK32)) {
-            return { ugFmtTo, ugCspTo };
+        case D3D9Format::A8R8G8B8:
+        case D3D9Format::X8R8G8B8: {
+          if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+            VkSurfaceFormatKHR ret;
+
+            switch(upgradeFormatTo)
+            {
+              case VK_FORMAT_R16G16B16A16_SFLOAT:
+              case VK_FORMAT_R16G16B16A16_UNORM:
+              case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
+              case VK_FORMAT_A2R10G10B10_UNORM_PACK32:
+              case VK_FORMAT_R8G8B8A8_UNORM:
+              case VK_FORMAT_B8G8R8A8_UNORM: {
+                ret = { upgradeFormatTo, upgradeColorSpaceTo };
+              } break;
+              case VK_FORMAT_MAX_ENUM: {
+                ret = { VK_FORMAT_B8G8R8A8_UNORM, upgradeColorSpaceTo };
+              } break;
+              default: {
+                SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_B8G8R8A8_UNORM);
+                ret = { VK_FORMAT_B8G8R8A8_UNORM, m_colorspace };
+              } break;
+            }
+
+            SwapChainUpgradeLogger(VK_FORMAT_B8G8R8A8_UNORM,
+                                   upgradeFormatTo,
+                                   upgradeColorSpaceTo);
+
+            return ret;
+          }
+          else {
+            return { VK_FORMAT_B8G8R8A8_UNORM, m_colorspace };
+          }
         }
-        else {
-          return { VK_FORMAT_B8G8R8A8_UNORM, m_colorspace };
-        }
-      }
 
       case D3D9Format::A8B8G8R8:
       case D3D9Format::X8B8G8R8: {
-        if (m_parent->GetOptions()->enableSwapchainUpgrade
-         && (ugFmtTo == VK_FORMAT_R16G16B16A16_SFLOAT
-          || ugFmtTo == VK_FORMAT_R16G16B16A16_UNORM
-          || ugFmtTo == VK_FORMAT_A2B10G10R10_UNORM_PACK32
-          || ugFmtTo == VK_FORMAT_A2R10G10B10_UNORM_PACK32)) {
-            return { ugFmtTo, ugCspTo };
+        if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+          VkSurfaceFormatKHR ret;
+
+          switch(upgradeFormatTo)
+          {
+            case VK_FORMAT_R16G16B16A16_SFLOAT:
+            case VK_FORMAT_R16G16B16A16_UNORM:
+            case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
+            case VK_FORMAT_A2R10G10B10_UNORM_PACK32:
+            case VK_FORMAT_R8G8B8A8_UNORM:
+            case VK_FORMAT_B8G8R8A8_UNORM: {
+              ret = { upgradeFormatTo, upgradeColorSpaceTo };
+            } break;
+            case VK_FORMAT_MAX_ENUM: {
+              ret = { VK_FORMAT_R8G8B8A8_UNORM, upgradeColorSpaceTo };
+            } break;
+            default: {
+              SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_R8G8B8A8_UNORM);
+              ret = { VK_FORMAT_R8G8B8A8_UNORM, m_colorspace };
+            } break;
+          }
+
+          SwapChainUpgradeLogger(VK_FORMAT_R8G8B8A8_UNORM,
+                                 upgradeFormatTo,
+                                 upgradeColorSpaceTo);
+
+          return ret;
         }
         else {
           return { VK_FORMAT_R8G8B8A8_UNORM, m_colorspace };
@@ -1192,12 +1255,31 @@ namespace dxvk {
       }
 
       case D3D9Format::A2R10G10B10: {
-        if (m_parent->GetOptions()->enableSwapchainUpgrade
-         && (ugFmtTo == VK_FORMAT_R16G16B16A16_SFLOAT
-          || ugFmtTo == VK_FORMAT_R16G16B16A16_UNORM
-          || ugFmtTo == VK_FORMAT_A2B10G10R10_UNORM_PACK32
-          || ugFmtTo == VK_FORMAT_A2R10G10B10_UNORM_PACK32)) {
-            return { ugFmtTo, ugCspTo };
+        if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+          VkSurfaceFormatKHR ret;
+
+          switch(upgradeFormatTo)
+          {
+            case VK_FORMAT_R16G16B16A16_SFLOAT:
+            case VK_FORMAT_R16G16B16A16_UNORM:
+            case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
+            case VK_FORMAT_A2R10G10B10_UNORM_PACK32: {
+              ret = { upgradeFormatTo, upgradeColorSpaceTo };
+            } break;
+            case VK_FORMAT_MAX_ENUM: {
+              ret = { VK_FORMAT_A2R10G10B10_UNORM_PACK32, upgradeColorSpaceTo };
+            } break;
+            default: {
+              SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_A2R10G10B10_UNORM_PACK32);
+              ret = { VK_FORMAT_A2R10G10B10_UNORM_PACK32, m_colorspace };
+            } break;
+          }
+
+          SwapChainUpgradeLogger(VK_FORMAT_A2R10G10B10_UNORM_PACK32,
+                                 upgradeFormatTo,
+                                 upgradeColorSpaceTo);
+
+          return ret;
         }
         else {
           return { VK_FORMAT_A2R10G10B10_UNORM_PACK32, m_colorspace };
@@ -1205,15 +1287,64 @@ namespace dxvk {
       }
 
       case D3D9Format::A2B10G10R10: {
-        if (m_parent->GetOptions()->enableSwapchainUpgrade
-         && (ugFmtTo == VK_FORMAT_R16G16B16A16_SFLOAT
-          || ugFmtTo == VK_FORMAT_R16G16B16A16_UNORM
-          || ugFmtTo == VK_FORMAT_A2B10G10R10_UNORM_PACK32
-          || ugFmtTo == VK_FORMAT_A2R10G10B10_UNORM_PACK32)) {
-            return { ugFmtTo, ugCspTo };
+        if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+          VkSurfaceFormatKHR ret;
+
+          switch(upgradeFormatTo)
+          {
+            case VK_FORMAT_R16G16B16A16_SFLOAT:
+            case VK_FORMAT_R16G16B16A16_UNORM:
+            case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
+            case VK_FORMAT_A2R10G10B10_UNORM_PACK32: {
+              ret = { upgradeFormatTo, upgradeColorSpaceTo };
+            } break;
+            case VK_FORMAT_MAX_ENUM: {
+              ret = { VK_FORMAT_A2B10G10R10_UNORM_PACK32, upgradeColorSpaceTo };
+            } break;
+            default: {
+              SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_A2B10G10R10_UNORM_PACK32);
+              ret = { VK_FORMAT_A2B10G10R10_UNORM_PACK32, m_colorspace };
+            } break;
+          }
+
+          SwapChainUpgradeLogger(VK_FORMAT_A2B10G10R10_UNORM_PACK32,
+                                 upgradeFormatTo,
+                                 upgradeColorSpaceTo);
+
+          return ret;
         }
         else {
           return { VK_FORMAT_A2B10G10R10_UNORM_PACK32, m_colorspace };
+        }
+      }
+
+      case D3D9Format::A16B16G16R16: {
+        if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+          VkSurfaceFormatKHR ret;
+
+          switch(upgradeFormatTo)
+          {
+            case VK_FORMAT_R16G16B16A16_SFLOAT:
+            case VK_FORMAT_R16G16B16A16_UNORM: {
+              ret = { upgradeFormatTo, upgradeColorSpaceTo };
+            } break;
+            case VK_FORMAT_MAX_ENUM: {
+              ret = { VK_FORMAT_R16G16B16A16_UNORM, upgradeColorSpaceTo };
+            } break;
+            default: {
+              SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_R16G16B16A16_UNORM);
+              ret = { VK_FORMAT_R16G16B16A16_UNORM, m_colorspace };
+            } break;
+          }
+
+          SwapChainUpgradeLogger(VK_FORMAT_R16G16B16A16_UNORM,
+                                 upgradeFormatTo,
+                                 upgradeColorSpaceTo);
+
+          return ret;
+        }
+        else {
+          return { VK_FORMAT_R16G16B16A16_UNORM, m_colorspace };
         }
       }
 
@@ -1225,14 +1356,37 @@ namespace dxvk {
         return { VK_FORMAT_R5G6B5_UNORM_PACK16, m_colorspace };
 
       case D3D9Format::A16B16G16R16F: {
-        if (!m_parent->HasFormatsUnlocked()) {
-          Logger::warn(str::format("D3D9SwapChainEx: Unexpected format: ", format));
-          return VkSurfaceFormatKHR { };
-        }
+        if (m_parent->GetOptions()->enableSwapChainUpgrade) {
+          VkSurfaceFormatKHR ret;
 
-        return { VK_FORMAT_R16G16B16A16_SFLOAT, m_colorspace };
+          switch(upgradeFormatTo)
+          {
+            case VK_FORMAT_R16G16B16A16_SFLOAT: {
+              ret = { upgradeFormatTo, upgradeColorSpaceTo };
+            } break;
+            case VK_FORMAT_MAX_ENUM: {
+              ret = { VK_FORMAT_R16G16B16A16_SFLOAT, upgradeColorSpaceTo };
+            } break;
+            default: {
+              SWAP_CHAIN_UPGRADE_THROW_ERROR(VK_FORMAT_R16G16B16A16_SFLOAT);
+              ret = { VK_FORMAT_R16G16B16A16_SFLOAT, m_colorspace };
+            } break;
+          }
+
+          SwapChainUpgradeLogger(VK_FORMAT_R16G16B16A16_SFLOAT,
+                                 upgradeFormatTo,
+                                 upgradeColorSpaceTo);
+
+          return ret;
+        }
+        else {
+          return { VK_FORMAT_R16G16B16A16_SFLOAT, m_colorspace };
+        }
       }
     }
+
+    #undef SWAP_CHAIN_UPGRADE_THROW_ERROR
+
   }
 
 
