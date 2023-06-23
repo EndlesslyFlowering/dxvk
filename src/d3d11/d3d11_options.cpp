1491b5c00ba62f2711c0c2495b10dda0e65abf0f
Lilium <EndlesslyFlowering@protonmail.com>
2023-06-23 18:58:04 +0200

    [d3d11] fix resource view, unordered access view and render target view upgrades being in the wrong place which didn't upgrade the format in all needed cases...

--- src/d3d11/d3d11_options.cpp
+++ src/d3d11/d3d11_options.cpp
@@ -347,7 +347,7 @@ namespace dxvk {
     this->reproducibleCommandStream     = config.getOption<bool>("d3d11.reproducibleCommandStream", false);
 
     // HDR-mod options start
-    this->enableRenderTargetUpgrade     = config.getOption<bool>("d3d11.enableRenderTargetUpgrade",     false);
+    this->enableRenderTargetUpgrades    = config.getOption<bool>("d3d11.enableRenderTargetUpgrades",    false);
     this->enableBackBufferFormatUpgrade = config.getOption<bool>("d3d11.enableBackBufferFormatUpgrade", false);
     this->enableSwapChainUpgrade        = config.getOption<bool>("d3d11.enableSwapChainUpgrade",        false);
     this->logRenderTargetFormatsUsed    = config.getOption<bool>("d3d11.logRenderTargetFormatsUsed",    false);
--- src/d3d11/d3d11_options.h
+++ src/d3d11/d3d11_options.h
@@ -143,7 +143,7 @@ namespace dxvk {
     bool exposeDriverCommandLists;
 
     /// enable render target upgrades
-    bool enableRenderTargetUpgrade;
+    bool enableRenderTargetUpgrades;
 
     /// array that describes which format will be ugprade to which
     std::array<FormatUpgradeInfo, 116> formatUpgradeInfoArray =
