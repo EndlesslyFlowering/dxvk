4f67ba5aac9261ff52111664dd76d2e447c5e4bd
Lilium <EndlesslyFlowering@protonmail.com>
2023-07-30 05:00:40 +0200

    [d3d11] add option to log views

--- src/d3d11/d3d11_options.h
+++ src/d3d11/d3d11_options.h
@@ -401,6 +401,9 @@ namespace dxvk {
     /// log formats used
     bool logRenderTargetFormatsUsed;
 
+    /// log all views used
+    bool logViewsOfRenderTargets;
+
     /// Ensure that for the same D3D commands the output VK commands
     /// don't change between runs. Useful for comparative benchmarking,
     /// can negatively affect performance.
@@ -413,5 +416,5 @@ namespace dxvk {
     /// Shader dump path
     std::string shaderDumpPath;
   };
-  
+
 }
