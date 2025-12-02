bfd3a65404bc7c3b2351cba3f0cf2afe957dc23d
Lilium <EndlesslyFlowering@protonmail.com>
2025-12-02 10:21:12 +0100

    [d3d9] call "IsSupportedBackBufferFormat" when with the backbuffer format when "IsSupportedBackBufferFormat" with windowed = true is called

--- src/d3d9/d3d9_monitor.cpp
+++ src/d3d9/d3d9_monitor.cpp
@@ -66,14 +66,7 @@ namespace dxvk {
           || (AdapterFormat == D3D9Format::R5G6B5        && BackBufferFormat == D3D9Format::R5G6B5);
     }
 
-    // D3D9Format::A2R10G10B10 is not advertised by native
-    // drivers as a windowed backbuffer format.
-    return BackBufferFormat == D3D9Format::A8R8G8B8
-        || BackBufferFormat == D3D9Format::X8R8G8B8
-        || BackBufferFormat == D3D9Format::A1R5G5B5
-        || BackBufferFormat == D3D9Format::X1R5G5B5
-        || BackBufferFormat == D3D9Format::R5G6B5
-        || BackBufferFormat == D3D9Format::Unknown;
+    return IsSupportedBackBufferFormat(BackBufferFormat);
   }
 
   bool IsSupportedBackBufferFormat(
